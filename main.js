ba7eac650b115e0efc54c74fc17af1b615b1cb92
Adam <adomas@wp.pl>
2020-08-18 12:34:28 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+alert   ('Witam na nieoficjalnej stronie PWSZ w Suwałkach');
+
+
+const firtName ='Adam';
+const age = 35;
+
+const learningTime = 4;
+const guestName = 'Sprytny Zbyś';
+
+console.log(firtName);
+console.log(age);
+
+console.log(`Siema mam na imię ${firtName} i mam ${age} lat.`);
+
+console.log(`${guestName} to ja zrobiłem tą zajebistą stronę, mino że uczę się dopiero ${learningTime} tygodnie;]`);
