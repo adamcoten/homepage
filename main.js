96c02f7bb665778b729a3e60576107b177fd32b6
Adam <adomas@wp.pl>
2020-08-22 13:34:49 +0200

    index navigation style in BEM

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-alert   ('Witam na nieoficjalnej stronie PWSZ w Suwałkach');
-
-
-const firtName ='Adam';
+const firtName = "Adam";
 const age = 35;
 
 const learningTime = 4;
-const guestName = 'Sprytny Zbyś';
+const guestName = "Sprytny Zbyś";
 
 console.log(firtName);
 console.log(age);
 
 console.log(`Siema mam na imię ${firtName} i mam ${age} lat.`);
 
-console.log(`${guestName} to ja zrobiłem tą zajebistą stronę, mino że uczę się dopiero ${learningTime} tygodnie;]`);
+console.log(
+  `${guestName} to ja zrobiłem tą zajebistą stronę, mino że uczę się dopiero ${learningTime} tygodnie;]`
+);
